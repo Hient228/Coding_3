33198e1fd1d76423d387460c2cc6892a35f462fd
Hient <hient@usf.edu>
2025-01-31 23:25:05 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -3,3 +3,10 @@ let prices = [17, 22, 6, 101, 8];
 prices.push(5);
 prices.shift();
 console.log("Updated Prices:",prices);
+
+//Task 2: Modifying Customer Orders 
+let orders = [10, 16, 25, 60, 35];
+orders [2]+=5;
+let totalOrders = orders.reduce((sum, order) => sum + order, 0); 
+console.log("Updated Orders:", orders);
+console.log("Total Order Count:", totalOrders);
